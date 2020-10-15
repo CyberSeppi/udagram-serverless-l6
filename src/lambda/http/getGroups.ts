59284085c3252475be8366913be237e6cece7f35
Cyberseppi <Julian.Prentl@bmw.de>
2020-10-15 17:21:33 +0200

    works now

--- src/lambda/http/getGroups.ts
+++ src/lambda/http/getGroups.ts
@@ -12,19 +12,13 @@ app.get('/groups', async (req, res) => {
 
     const groups = await getAllGroups()
 
-    console.log('Request: ', req)
-    console.log('Resp (whatś this?): ', res)
     console.log('Retrieved Groups:', groups)
 
-    return {
-        status: 200,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Credentials': true
-        },
-        body: JSON.stringify({
-            items: groups
-        })
-    }
+   
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.status('200')
+    res.json({
+        items: groups
+      })
 })
 exports.handler = (event, context) => { awsServerlessExpress.proxy(server, event, context) }
